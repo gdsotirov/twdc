9a72f8f27c0374c79d3ffbe48f40fc0a2b51c8e6
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-04-20 19:17:57 +0000

    Initial version

---
+++ client.c
@@ -0,0 +1,13 @@
+/* Client which send files to a server
+ * ---
+ * Written by George D. Sotirov <gdsotirov@dir.bg>
+ * $Id: client.c,v 1.1 2005/04/20 19:17:57 gsotirov Exp $
+ */
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+
+  return 0;
+}
+
---
+++ server.c
@@ -0,0 +1,12 @@
+/* Server which receive files from clients
+ * ---
+ * Written by George D. Sotirov <gdsotirov@dir.bg>
+ * $Id: server.c,v 1.1 2005/04/20 19:17:57 gsotirov Exp $
+ */
+
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+
+  return 0;
+}
