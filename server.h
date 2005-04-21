2a53cc664876a41d484de5cf35e616a37ad6a95f
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-04-21 19:24:50 +0000

    Add server header

---
+++ server.h
@@ -0,0 +1,39 @@
+/* TWT_Server - a client programm for the Tumbleweed Developer's Task
+ * Copyright (C) 2005 Georgi D. Sotirov 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+/* Title: Server for the Tumbleweed Developer's Contest Task
+ * Description: Server configuraiton
+ * File: server.h
+ * ---
+ * Written by George D. Sotirov <gdsotirov@dir.bg>
+ * $Id: server.h,v 1.1 2005/04/21 19:24:50 gsotirov Exp $
+ */
+
+#ifndef __TWT_SERVER_H__
+#define __TWT_SERVER_H__
+
+#define BACKLOG 10
+
+/* Maximum file size in bytes the server should accept
+ * Default: 100 MB
+ */
+#define MAXFILESIZE 104857600
+
+#endif
+
