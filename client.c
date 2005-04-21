b2aed8239ca28dab274b5cf123176caecbfa3c26
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-04-21 19:17:48 +0000

    Add GPL info

--- client.c
+++ client.c
@@ -1,7 +1,28 @@
-/* Client which send files to a server
+/* TWT_Client - a client programm for the Tumbleweed Developer's Task
+ * Copyright (C) 2005 Georgi D. Sotirov 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+/* Title: Client for the Tumbleweed Developer's Contest Task
+ * Description: Client main programm
+ * File: client.c
  * ---
  * Written by George D. Sotirov <gdsotirov@dir.bg>
- * $Id: client.c,v 1.1 2005/04/20 19:17:57 gsotirov Exp $
+ * $Id: client.c,v 1.2 2005/04/21 19:17:48 gsotirov Exp $
  */
 
 #include <stdio.h>
