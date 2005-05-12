a17c54259a0e977ca430d35b59e76e507ffe7721
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-05-12 18:00:35 +0000

    Reflect header update (use new data member, make all function arguments const)

--- protocol.c
+++ protocol.c
@@ -1,5 +1,5 @@
 /* TWDC - a client/server application for the Tumbleweed Developer's Contest
- * Copyright (C) 2005 Georgi D. Sotirov 
+ * Copyright (C) 2005 Georgi D. Sotirov
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,7 +22,7 @@
  * File: protocol.c
  * ---
  * Written by George D. Sotirov <gdsotirov@dir.bg>
- * $Id: protocol.c,v 1.2 2005/05/11 21:23:56 gsotirov Exp $
+ * $Id: protocol.c,v 1.3 2005/05/12 18:00:35 gsotirov Exp $
  */
 
 #include <string.h>
@@ -31,7 +31,7 @@
 
 #include "protocol.h"
 
-int get_msg_type(struct twdc_msg_head * msg) {
+int get_msg_type(const struct twdc_msg_head * msg) {
   if ( msg != NULL )
     return msg->msg_type;
   return 0;
@@ -54,7 +54,7 @@ void set_version(struct twdc_msg_head * head) {
  *               check_type - the type of the check, one of CT_EXACT, CT_GT,
  *                            CT_GTOREQ, CT_LT, CT_LTOREQ
  */
-int check_version_maj(struct twdc_msg_head * msg, uint8_t ver_major, int check_type) {
+int check_version_maj(const struct twdc_msg_head * msg, const uint8_t ver_major, const int check_type) {
   if ( msg != NULL ) {
     if ( check_type == CT_EXACT )
       return (ver_major == msg->ver_major);
@@ -77,7 +77,7 @@ int check_version_maj(struct twdc_msg_head * msg, uint8_t ver_major, int check_t
  *               check_type - the type of the check, one of CT_EXACT, CT_GT,
  *                            CT_GTOREQ, CT_LT, CT_LTOREQ
  */
-int check_version_min(struct twdc_msg_head * msg, uint8_t ver_minor, int check_type) {
+int check_version_min(const struct twdc_msg_head * msg, const uint8_t ver_minor, const int check_type) {
   if ( msg != NULL ) {
     if ( check_type == CT_EXACT )
       return (ver_minor == msg->ver_minor);
@@ -93,10 +93,18 @@ int check_version_min(struct twdc_msg_head * msg, uint8_t ver_minor, int check_t
   return 0; /* false */
 }
 
+/* Function    : get_ver_info
+ * Description : Extract protocol version info for a message
+ */
+void get_ver_info(const struct twdc_msg_head * msg, int8_t * ver_maj, int8_t * ver_min) {
+  *ver_maj = msg->ver_major;
+  *ver_min = msg->ver_minor;
+}
+
 /* Function    : make_err_msg
  * Description : Init an error message
  */
-void make_err_msg(struct twdc_msg * msg, int8_t err_cd, ...) {
+void make_err_msg(struct twdc_msg * msg, const int8_t err_cd, ...) {
   if ( msg != NULL ) {
     va_list vl;
 
@@ -106,12 +114,12 @@ void make_err_msg(struct twdc_msg * msg, int8_t err_cd, ...) {
 
     va_start(vl, err_cd);
     switch ( err_cd ) {
-      case TWDC_ERR_FILE_SZ: {
-        uint32_t max_sz = va_arg(vl, uint32_t);
-        memcpy(msg->body.error.data, (void *)htonl(max_sz), sizeof(msg->body.error.data));
-
-      }
-      break;
+      /* The extra information to the file size error message will be
+       * the max file size which the server can accept.
+       */
+      case TWDC_ERR_FILE_SZ :
+        msg->body.error.data.max_sz = htonl(va_arg(vl, uint32_t));
+        break;
     default: break;
     }
     va_end(vl);
@@ -121,7 +129,7 @@ void make_err_msg(struct twdc_msg * msg, int8_t err_cd, ...) {
 /* Function    : make_file_msg
  * Description : Init an file request message
  */
-void make_file_msg(struct twdc_msg * msg, char * fname, size_t fsize) {
+void make_file_msg(struct twdc_msg * msg, const char * fname, const size_t fsize) {
   if ( msg != NULL ) {
     msg->header.msg_type = TWDC_MSG_FILE_REQ;
     set_version(&msg->header);
@@ -134,7 +142,7 @@ void make_file_msg(struct twdc_msg * msg, char * fname, size_t fsize) {
 /* Function    : make_data_msg
  * Description : Init an data message
  */
-void make_data_msg(struct twdc_msg * msg, char * buf, size_t buf_sz) {
+void make_data_msg(struct twdc_msg * msg, const char * buf, const size_t buf_sz) {
   if ( msg != NULL ) {
     msg->header.msg_type = TWDC_MSG_DATA;
     set_version(&msg->header);
@@ -143,22 +151,31 @@ void make_data_msg(struct twdc_msg * msg, char * buf, size_t buf_sz) {
   }
 }
 
+/* Function    : get_err_code
+ * Description : Retrieve error code from an error message
+ */
+int8_t get_err_code(const struct twdc_msg * msg) {
+  if ( msg != NULL )
+    return msg->body.error.err_code;
+  return 0;
+}
+
 /* Function    : read_err_msg
  * Description : Retrive information from an error message
  */
-void read_err_msg(struct twdc_msg * msg, int8_t * err_cd, ...) {
+void read_err_msg(const struct twdc_msg * msg, ...) {
+  int8_t err_cd = 0;
+
   if ( msg != NULL ) {
     va_list vl;
 
-    if ( err_cd != NULL )
-      *err_cd = msg->body.error.err_code;
-
-    va_start(vl, err_cd);
-    switch ( *err_cd ) {
-      case TWDC_ERR_FILE_SZ: {
-          uint32_t * max_sz = NULL;
-          if ( (max_sz = va_arg(vl, uint32_t *)) != NULL )
-            memcpy(max_sz, (void *)ntohl((uint32_t)msg->body.error.data), sizeof(max_sz));
+    err_cd = msg->body.error.err_code;
+    va_start(vl, msg);
+    switch ( err_cd ) {
+      case TWDC_ERR_FILE_SZ : {
+          uint32_t * max_sz = va_arg(vl, uint32_t *);
+          if ( max_sz != NULL )
+            *max_sz = ntohl(msg->body.error.data.max_sz);
         }
         break;
       default: break;
@@ -170,7 +187,7 @@ void read_err_msg(struct twdc_msg * msg, int8_t * err_cd, ...) {
 /* Function    : read_file_msg
  * Description : Retrive information from an file request message
  */
-void read_file_msg(struct twdc_msg * msg, char * fname, size_t fname_sz, size_t * fsize) {
+void read_file_msg(const struct twdc_msg * msg, char * fname, const size_t fname_sz, size_t * fsize) {
   if ( msg != NULL ) {
     if ( fname != NULL )
       strncpy(fname, msg->body.file.fname, fname_sz);
