72645b217c630ffca7c876614231532bebc23e26
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-05-14 22:20:31 +0000

    Error messages reorganization

--- client.h
+++ client.h
@@ -22,40 +22,45 @@
  * File: client.h
  * ---
  * Written by George D. Sotirov <gdsotirov@dir.bg>
- * $Id: client.h,v 1.7 2005/05/13 17:31:50 gsotirov Exp $
+ * $Id: client.h,v 1.8 2005/05/14 22:20:31 gsotirov Exp $
  */
 
 #ifndef __TWDC_CLIENT_H__
 #define __TWDC_CLIENT_H__
 
+#include "protocol.h"
+
 /* client error codes */
 /* NOTE: If you add code here do not forget to add it in the switch operator
  *       of print_error function.
  */
-#define ERR_FNAME_TOO_LONG   -100
-#define ERR_HOSTNM_TOO_LONG  -101
-#define ERR_NO_FILE_GIVEN    -102
-#define ERR_INVLD_FILE_NM    -103
-#define ERR_NO_HOST_GIVEN    -104
-#define ERR_CNT_RSLVE_HOST   -105
-#define ERR_INVLD_AF         -106
-#define ERR_CNT_RD_FILE      -201
-#define ERR_CNT_GET_FILE_SZ  -202
-#define ERR_CNT_OPEN_SOCK    -203
-#define ERR_CNT_CNNCT_HOST   -204
-#define ERR_SND_DATA         -205
-#define ERR_RCV_DATA         -206
-#define ERR_SRV_UNKNWN       -301
-#define ERR_SRV_PROTO_VER    -302
-#define ERR_SRV_UNEXPCTD     -303
-#define ERR_SRV_FILE_SZ      -304
+#define ERR_FPATH_ERR       -101
+#define ERR_FNAME_ERR       -102
+#define ERR_HOSTNM_ERR      -103
+#define ERR_NO_FILE_GIVEN   -104
+#define ERR_INVLD_FILE_NM   -105
+#define ERR_NO_HOST_GIVEN   -106
+#define ERR_CNT_RSLVE_HOST  -107
+#define ERR_INVLD_AF        -108
+#define ERR_CNT_RD_FILE     -201
+#define ERR_CNT_GET_FILE_SZ -202
+#define ERR_CNT_OPEN_SOCK   -203
+#define ERR_CNT_CNNCT_HOST  -204
+#define ERR_SND_DATA        -205
+#define ERR_RCV_DATA        -206
+#define ERR_SRV_UNKNWN      -301
+#define ERR_SRV_PROTO_VER   -302
+#define ERR_SRV_FILE_SZ     -303
+#define ERR_SRV_FILE_NM     -304
+#define ERR_SRV_SYS         -305
 
 /* client warning codes */
 #define WARN_ZERO_FILE        100
 
 /* client error messages */
-#define ERR_FNAME_TOO_LONG_STR  "File name too long. The length should not exceed %d characters"
-#define ERR_HOSTNM_TOO_LONG_STR "Host name too long. The length should not exceed %d characters"
+#define ERR_FPATH_ERR_STR       "Either no path to file given or it is too long. The length should not exceed %d characters"
+#define ERR_FNAME_ERR_STR       "Either no filename given or it is too long. The length should not exceed %d characters"
+#define ERR_HOSTNM_ERR_STR      "Either no host name given or it is too long. The length should not exceed %d characters"
 #define ERR_NO_FILE_GIVEN_STR   "Please, specify file to upload"
 #define ERR_INVLD_FILE_NM_STR   "Invalid file name '%s'"
 #define ERR_NO_HOST_GIVEN_STR   "Please, specify server host"
@@ -67,10 +72,11 @@
 #define ERR_CNT_CNNCT_HOST_STR  "Can not connect to %s:%hd (%s:%hd)"
 #define ERR_SND_DATA_STR        "While sending data to '%s:%hd'"
 #define ERR_RCV_DATA_STR        "While receiving data from '%s:%hd'"
-#define ERR_SRV_UNKNWN_STR      "Server: Unknown error"
-#define ERR_SRV_PROTO_VER_STR   "Server: Your client protocol version %d.%d is not supported. The server supports protocol versions prior or equal to %d.%d"
-#define ERR_SRV_UNEXPCTD_STR    "Server: Unexpected message"
-#define ERR_SRV_FILE_SZ_STR     "Server: Sorry. File size too long. It should not exceed %d Bytes (%s)"
+#define ERR_SRV_UNKNWN_STR      "Unknown error code %d"
+#define ERR_SRV_PROTO_VER_STR   TWDC_ERR_PROTO_VER_STR
+#define ERR_SRV_FILE_SZ_STR     TWDC_ERR_FILE_SZ_STR
+#define ERR_SRV_FILE_NM_STR     TWDC_ERR_FILE_NM_STR
+#define ERR_SRV_SYS_STR         TWDC_ERR_PROTO_VER_STR
 
 /* client warning messages */
 #define WARN_ZERO_FILE_STR      "The transfered file is with zero size."
