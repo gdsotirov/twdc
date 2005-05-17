c8442de9b4d8b8e85ec660ec5d6100e61df0c5fe
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-05-17 20:14:44 +0000

    Add new error messages

--- client.h
+++ client.h
@@ -22,7 +22,7 @@
  * File: client.h
  * ---
  * Written by George D. Sotirov <gdsotirov@dir.bg>
- * $Id: client.h,v 1.8 2005/05/14 22:20:31 gsotirov Exp $
+ * $Id: client.h,v 1.9 2005/05/17 20:14:44 gsotirov Exp $
  */
 
 #ifndef __TWDC_CLIENT_H__
@@ -34,6 +34,7 @@
 /* NOTE: If you add code here do not forget to add it in the switch operator
  *       of print_error function.
  */
+#define ERR_OK              0
 #define ERR_FPATH_ERR       -101
 #define ERR_FNAME_ERR       -102
 #define ERR_HOSTNM_ERR      -103
@@ -48,6 +49,11 @@
 #define ERR_CNT_CNNCT_HOST  -204
 #define ERR_SND_DATA        -205
 #define ERR_RCV_DATA        -206
+#define ERR_WR_DATA         -207
+#define ERR_RD_DATA         -208
+#define ERR_PIPE            -209
+#define ERR_CHILD           -210
+#define ERR_ZLIB            -211
 #define ERR_SRV_UNKNWN      -301
 #define ERR_SRV_PROTO_VER   -302
 #define ERR_SRV_FILE_SZ     -303
@@ -66,12 +72,17 @@
 #define ERR_NO_HOST_GIVEN_STR   "Please, specify server host"
 #define ERR_CNT_RSLVE_HOST_STR  "Can not resolve host '%s'"
 #define ERR_INVLD_AF_STR        "Client does not support the address family of the local host"
-#define ERR_CNT_RD_FILE_STR     "Can not read from file '%s'"
-#define ERR_CNT_GET_FILE_SZ_STR "Can not get the size of file '%s'"
-#define ERR_CNT_OPEN_SOCK_STR   "Can not open socket"
-#define ERR_CNT_CNNCT_HOST_STR  "Can not connect to %s:%hd (%s:%hd)"
+#define ERR_CNT_RD_FILE_STR     "Cannot read from file '%s'"
+#define ERR_CNT_GET_FILE_SZ_STR "Cannot get the size of file '%s'"
+#define ERR_CNT_OPEN_SOCK_STR   "Cannot open socket"
+#define ERR_CNT_CNNCT_HOST_STR  "Cannot connect to %s:%hd (%s:%hd)"
 #define ERR_SND_DATA_STR        "While sending data to '%s:%hd'"
 #define ERR_RCV_DATA_STR        "While receiving data from '%s:%hd'"
+#define ERR_WR_DATA_STR         "While writting data"
+#define ERR_RD_DATA_STR         "While reading data"
+#define ERR_PIPE_STR            "Cannot create pipe"
+#define ERR_CHILD_STR           "Cannot create child process"
+#define ERR_ZLIB_STR            "While compressing data"
 #define ERR_SRV_UNKNWN_STR      "Unknown error code %d"
 #define ERR_SRV_PROTO_VER_STR   TWDC_ERR_PROTO_VER_STR
 #define ERR_SRV_FILE_SZ_STR     TWDC_ERR_FILE_SZ_STR
